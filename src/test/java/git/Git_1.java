e5e716ec4334a4929ba0e9143f6d0d7025cee980
Shiva_kc8lfne <Shiva_kc8lfne@DESKTOP-6G64E8M>
2023-12-26 16:14:09 +0530

    project 3 added

---
+++ src/test/java/git/Git_1.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Git_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Project added");
+
+	}
+
+}
---
+++ src/test/java/git/Git_pro2.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Git_pro2 {
+
+	public static void main(String[] args) {
+		System.out.println("2nd programadded");
+
+	}
+
+}
